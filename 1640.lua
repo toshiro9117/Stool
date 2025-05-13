35e0f6ea01eb2cf571257542ab9ff72d0e811039
oureveryday <oureveryday@163.com>
2025-05-14 01:09:34 +0800

    Update 1640 manifest files

--- 1640.lua
+++ 1640.lua
@@ -1,3 +1,7 @@
 addappid(1640)
+addappid(1641)
+addappid(1642)
+addappid(1643)
+addappid(1644)
 addappid(1004,0,"f21faa4f29ecee9d192fc1784d1701376eb87e13521824ceaf0116b10c558a81")
 setManifestid(1004,"7747775778018907449")
