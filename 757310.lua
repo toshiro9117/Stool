1af574db14bbf9782bfa99cc81a98f98b1fd4f66
oureveryday <oureveryday@163.com>
2025-05-19 12:28:53 +0800

    Update 757310 manifest files

--- 757310.lua
+++ 757310.lua
@@ -1,3 +1,5 @@
 addappid(757310)
 addappid(757311,0,"a6c014ff9375e39f5bff97ef45bce9d11ec4a288c85ccd82c8200b77ecdd9d5c")
 setManifestid(757311,"2532492630309877007")
+addappid(757312)
+addappid(757313)
