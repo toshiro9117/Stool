5063543aa24316bfb900024ee5bc388f6aef38ff
oureveryday <oureveryday@163.com>
2025-05-19 12:40:59 +0800

    Update 1955110 manifest files

--- 1955110.lua
+++ 1955110.lua
@@ -1,3 +1,6 @@
 addappid(1955110)
 addappid(1955111,0,"4deb7909fe04531db2c03abd6bebfa3d5d42eebdc63675908161bf1bda9bca88")
 setManifestid(1955111,"3352566018377732799")
+addappid(1955112)
+addappid(1955113)
+addappid(1955114)
