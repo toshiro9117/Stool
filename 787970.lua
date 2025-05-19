040aa316ae2e182704a07037dfe24522e182359b
oureveryday <oureveryday@163.com>
2025-05-19 14:12:19 +0800

    Update 787970 manifest files

--- 787970.lua
+++ 787970.lua
@@ -1,3 +1,8 @@
 addappid(787970)
+addappid(787972)
+addappid(787973)
+addappid(787974)
+addappid(787975)
+addappid(787976)
 addappid(787977,0,"724932ee03acd283cdb0914df322ffdceb3f426a18c2a32fe71469cb3fd3ff13")
 setManifestid(787977,"6106297228576065647")
