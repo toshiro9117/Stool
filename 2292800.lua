0f83595d69b5daccc0336dc40b85ae322aeb9ff1
oureveryday <oureveryday@163.com>
2025-05-19 12:25:36 +0800

    Update 2292800 manifest files

---
+++ 2292800.lua
@@ -0,0 +1,5 @@
+addappid(2292800)
+addappid(2292801,0,"eabaeaf0e86b550dc3dfb73dbfc87bf0ef141f3279e04f2e41ed631dc95c8f08")
+setManifestid(2292801,"6433129286351227455")
+addappid(2292802)
+addappid(2292803)
