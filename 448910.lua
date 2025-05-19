c06fcdd367d45009331be9fd996431da2313559c
oureveryday <oureveryday@163.com>
2025-05-19 14:27:47 +0800

    Update 448910 manifest files

--- 448910.lua
+++ 448910.lua
@@ -1,3 +1,5 @@
 addappid(448910)
 addappid(448911,0,"fb839ab641d1b7c8cdab95ba6a65e454daabbf7bf9c1abf483dd92fc751bda9c")
 setManifestid(448911,"1667228034481962119")
+addappid(448912)
+addappid(448913)
