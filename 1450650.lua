c134f6f65b84fd82c14b045b1c63238f98e862f6
oureveryday <oureveryday@163.com>
2025-05-19 13:42:20 +0800

    Update 1450650 manifest files

--- 1450650.lua
+++ 1450650.lua
@@ -1,3 +1,8 @@
 addappid(1450650)
+addappid(1450651)
+addappid(1450652)
+addappid(1450653)
 addappid(1450654,0,"5c11ffc40a31fdcf46e93251ac7567a7e2356c4763bfef17e8c30e06c3c504b2")
 setManifestid(1450654,"2739598954615317302")
+addappid(1450655)
+addappid(1450656)
