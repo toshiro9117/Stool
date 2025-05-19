ea462817a639e2822b163b552e4be0534e08d347
oureveryday <oureveryday@163.com>
2025-05-19 14:08:40 +0800

    Update 823500 manifest files

--- 823500.lua
+++ 823500.lua
@@ -1,3 +1,5 @@
 addappid(823500)
 addappid(823501,0,"bd62786d436b7bc794ccc6f50cdcfc4c47c11029fbbc4751de27d12fb8fe8622")
 setManifestid(823501,"5927460188866243002")
+addappid(823502)
+addappid(823503)
