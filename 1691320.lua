dd27a2955a05aa7614f96a66da046cf064379c0c
oureveryday <oureveryday@163.com>
2025-05-24 14:06:05 +0800

    Update 1691320 manifest files

--- 1691320.lua
+++ 1691320.lua
@@ -1,5 +1,6 @@
 addappid(1691320)
 addappid(228989)
+setManifestid(228989,"550968249685141759")
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
 addappid(1691321,0,"6ed7eab6be8aa8d09de38a1b99103fd5bce39cf844cae93a7ecae7abb4a9b902")
