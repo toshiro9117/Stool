d845a8a252ed9faa0dc8560fb1e9d4149ed803c9
oureveryday <oureveryday@163.com>
2025-05-24 13:57:51 +0800

    Update 3176210 manifest files

--- 3176210.lua
+++ 3176210.lua
@@ -1,5 +1,6 @@
 addappid(3176210)
 addappid(228989)
+setManifestid(228989,"550968249685141759")
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
 addappid(3176211,0,"27b242ff8edfcc2412b0a47fc5b8c608cd19afe5ac5c79db74b4bc1feede3836")
