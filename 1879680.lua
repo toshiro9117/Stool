99455c3b2a17065ced878b5f7533c200f0855e02
oureveryday <oureveryday@163.com>
2025-05-24 14:52:33 +0800

    Update 1879680 manifest files

--- 1879680.lua
+++ 1879680.lua
@@ -1,5 +1,6 @@
 addappid(1879680)
 addappid(228989)
+setManifestid(228989,"550968249685141759")
 addappid(228990)
 setManifestid(228990,"1829726630299308803")
 addappid(1879681,0,"9cb6a9082de4bdfdfe9e6872827ef9e5bccf28fee8038a03f841b7bc0f9f5fcd")
